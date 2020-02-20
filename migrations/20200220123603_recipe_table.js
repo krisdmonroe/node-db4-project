dc546a265e162fb08f130cf6fb96e9b5e0506b43
Kris Monroe <krisdmonroe@gmail.com>
2020-02-20 14:18:14 -0800

    table finished not perfect

--- migrations/20200220123603_recipe_table.js
+++ migrations/20200220123603_recipe_table.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable("recipes", tbl => {
-      tbl.increments();
+    tbl.increments();
     tbl.string('recipe_name' , 128)
       .unique()
       .notNullable();
@@ -9,10 +9,30 @@ exports.up = function(knex) {
   })
   .createTable("ingredients", tbl => {
       tbl.increments()
-      tbl.text("ingredient")
+      tbl.string("ingredient_name", 128).notNullable()
+  })
+  .createTable("recipe_step", tbl => {
+      tbl.increments()
+      tbl.float('quantity').notNullable()
+      tbl.integer("recipe_id")
+      .unsigned()
+      .notNullable()
+      .reference("id")
+      .intable("recipes")
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+      tbl.integer("ingredient_id")
+      .unsigned()
+      .notNullable()
+      .reference("id")
+      .intable("ingredients")
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
   })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("recipes")
+  .dropTableIfExists("ingredients")
+  .dropTableIfExists("recipe_step")
 };
