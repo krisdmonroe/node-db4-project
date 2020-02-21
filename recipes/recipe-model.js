87b2cca1cac07b47b99a7a4a8090322161732efc
Kris Monroe <krisdmonroe@gmail.com>
2020-02-20 16:01:43 -0800

    Stuck on shopping list

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -3,12 +3,25 @@ const db = require("./dbConfig.js")
 module.exports = {
     getRecipes,
     getShoppingList,
+    getInstructions,
 }
 
 
 function getRecipes(){
     return db("recipes")
 }
-function getShoppingList(recipe_id){
 
+function getShoppingList(id){
+    return db("ingredients")
+    .join("recipe_step", "recipe_step.ingredient_id" , "=", "ingredients.id")
+    .select("recipe_step.quantity","ingredients.ingredient_Name")
+    .where({ id })
+    .first()
+}
+
+function getInstructions(id){
+    return db("recipes")
+    .select("recipes.instructions")
+    .where({ id })
+    .first()
 }
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -15,5 +15,35 @@ router.get('/', (req, res) => {
     });
   });
   
+  router.get('/:id/list', (req, res) => {
+    const id = req.params.id;
+  
+    Recipes.getShoppingList(id)
+    .then(recipe => {
+        if (recipe) {
+          res.json(recipe);
+        } else {
+          res.status(404).json({ message: "Could not recipe with given id." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Failed to get recipe shopping list" });
+      });
+
+  });
+  router.get('/:id/in', (req, res) => {
+    const id = req.params.id;
+    Recipes.getInstructions(id)
+    .then(recipe => {
+        if (recipe) {
+          res.json(recipe);
+        } else {
+          res.status(404).json({ message: "Could not recipe with given id." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Failed to get recipe instructions" });
+      });
+  });
 
 module.exports = router;
